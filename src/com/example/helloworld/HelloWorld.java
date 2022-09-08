fa500a60a2e36abdf55bdc352396ce320406f686
Wojciech Kwapisiewicz <wojciech.kwapisiewicz@gmail.com>
2022-09-08 13:48:14 +0200

    Hello world implementatoin

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+//based on: https://www.jetbrains.com/help/idea/creating-and-running-your-first-java-application.html#write-code
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
